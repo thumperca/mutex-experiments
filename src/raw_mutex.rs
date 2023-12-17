1b5ce6bbd2b8ac13746f08acc888e7fdb935d81a
Manvir Singh <mail@manvir.me>
2023-12-17 15:12:41 +0530

    Improved raw mutex

--- src/raw_mutex.rs
+++ src/raw_mutex.rs
@@ -1,36 +1,73 @@
 use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::Relaxed;
+use std::sync::atomic::Ordering::{Acquire, Relaxed};
+use std::thread::sleep;
+use std::time::Duration;
 
-pub struct Mutex<'a, T> {
+pub struct Mutex<T> {
     lock: AtomicBool,
-    data: &'a mut T,
+    data: *mut T,
 }
 
-impl<'a, T> Mutex<'a, T> {
+impl<T> Mutex<T> {
     pub fn new(value: T) -> Self {
-        let value = Box::leak(Box::new(value));
+        let value = Box::into_raw(Box::new(value));
         Self {
             lock: AtomicBool::new(false),
             data: value,
         }
     }
 
-    // pub fn lock(&self) -> &mut T {
-    //     while let Err(_) = self.lock.compare_exchange(false, true, Acquire, Relaxed) {
-    //         sleep(Duration::from_nanos(10));
-    //     }
-    //     let d = unsafe { &mut *self.data };
-    //     d
-    // }
+    pub fn lock(&self) -> &mut T {
+        while let Err(_) = self.lock.compare_exchange(false, true, Acquire, Relaxed) {
+            sleep(Duration::from_nanos(10));
+        }
+        let d = unsafe { &mut *self.data };
+        d
+    }
 
-    pub fn unlock(&self) {
+    pub unsafe fn unlock(&self) {
         self.lock.store(false, Relaxed);
     }
 }
 
-impl<'a, T> Drop for Mutex<'a, T> {
+impl<T> Drop for Mutex<T> {
     fn drop(&mut self) {
         let value = unsafe { Box::from_raw(self.data) };
         drop(value);
+    }
+}
+
+unsafe impl<T> Send for Mutex<T> {}
+unsafe impl<T> Sync for Mutex<T> {}
+
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    #[test]
+    fn it_works() {
+        let mux = Arc::new(Mutex::new(100));
+        // update mutex
+        std::thread::scope(|scope| {
+            let m1 = mux.clone();
+            let m2 = mux.clone();
+            scope.spawn(move || {
+                let d = m1.lock();
+                *d += 1;
+                unsafe {
+                    m1.unlock();
+                }
+            });
+            scope.spawn(move || {
+                let d = m2.lock();
+                *d += 1;
+                unsafe {
+                    m2.unlock();
+                }
+            });
+        });
+        // check update
+        let d = mux.lock();
+        assert_eq!(102, *d);
     }
 }
